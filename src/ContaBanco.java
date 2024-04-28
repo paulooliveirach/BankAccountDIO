e17e013a02fe8e09b5a9d15e723abbcc1568397f
Paulo Reis <paulo.reis13@hotmail.com>
2024-04-28 11:31:48 +0200

    starting the challenge

---
+++ src/ContaBanco.java
@@ -0,0 +1,11 @@
+public class ContaBanco {
+    public static void main(String[] args) throws Exception {
+              //TODO: to know and import the "scanner" class.
+
+       //TODO: Show the mensages for our user
+
+       //TODO: get the values entered in the terminal by the class
+
+       // TODO: display the account created message
+    }
+}
